a3cc33feb27236eacf781e00edccd248bb9b901a
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-30 21:21:21 +0530

    Time: 25 ms (30.7%), Space: 48.1 MB (21.45%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,50 @@
 class Solution {
+    public int max(int[] arr) {
+        int max = 0;
+        for (int num : arr) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    public int calculateHrs(int[] arr, int h) {
+        int sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum += Math.ceil((double) arr[j] / h);
+        }
+        return sum;
+    }
+
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
-        if(h==n) return max(piles);
-        int low = 1;
-        int high = max(piles);
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int hours = calHrs(piles, mid);
-            if (hours <= h)
-                high = mid - 1;
+        int left = 1;
+        int right = max(piles);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int hrs = calculateHrs(piles, mid);
+            if (hrs <= h)
+                right = mid;
             else
-                low = mid + 1;
+                left = mid + 1;
         }
-        return low;
+        return left;
     }
 
-    int calHrs(int arr[], int val) {
-        int hr = 0;
-        for (int i : arr) {
-            hr += Math.ceil((double)i / val);
+    // brute force
+    public int bruteforce(int[] piles, int h) {
+        int max = 0;
+        int n = piles.length;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, piles[i]);
         }
-        return hr;
-    }
 
-    int max(int arr[]) {
-        int ans = Integer.MIN_VALUE;
-        for (int i : arr) {
-            ans = Math.max(ans, i);
+        for (int i = 1; i < max; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += Math.ceil((double) piles[j] / i);
+            }
+            if (sum <= h)
+                return i;
         }
-        return ans;
+        return max;
     }
 }
