1a2e56cb6955189b7cdf135384771828f76d5d74
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-01 15:33:46 +0530

    Time: 8 ms (99.72%), Space: 42.4 MB (99.14%) - LeetHub

---
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAllOneMultiple(int k) {
+        // No solution if k has factor 2 or 5
+        if (k % 2 == 0 || k % 5 == 0)
+            return -1;
+
+        int remainder = 0;
+        for (int i = 1; i <= k; i++) {
+            remainder = (remainder * 10 + 1) % k;
+            if (remainder == 0)
+                return i;
+        }
+
+        return -1;
+    }
+}
