16333b3bd8ec9d61c8558c9c81e03cd4621a0f66
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-12 00:45:46 +0530

    Time: 74 ms (34.46%), Space: 78.7 MB (28.5%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,52 +1,40 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<>(); // store indexes
+        int area = 0;
+        int maxArea = 0;
         int n = heights.length;
 
-        int[] nse = findNSE(heights);
-        int[] pse = findPSE(heights);
-
-        int maxArea = 0;
-
         for (int i = 0; i < n; i++) {
-            int width = nse[i] - pse[i] - 1;
-            int area = heights[i] * width;
-            maxArea = Math.max(maxArea, area);
+            // T.C. : O(N) + stack traversals : O(N)
+            // S.C. : O(N)
+            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
+                int elementIdx = st.peek(); // we'll calculate area for this one
+                st.pop();
+
+                int nse = i;
+                int pse = st.isEmpty() ? -1 : st.peek();
+
+                area = heights[elementIdx] * (nse - pse - 1);
+                maxArea = Math.max(area, maxArea);
+
+            }
+
+            st.push(i);
+        }
+
+        // what if elements are still left in the stack lets say the last one orr the smallest one
+        while (!st.isEmpty()) {
+            int nse = n;
+            int elementIdx = st.peek();
+            st.pop();
+
+            int pse = st.isEmpty() ? -1 : st.peek();
+
+            area = heights[elementIdx] * (nse - pse - 1);
+            maxArea = Math.max(area, maxArea);
         }
 
         return maxArea;
-    }
-
-    // Next Smaller Element index
-    private int[] findNSE(int[] heights) {
-        int n = heights.length;
-        int[] nse = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
-            }
-            nse[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
-        }
-
-        return nse;
-    }
-
-    // Previous Smaller Element index
-    private int[] findPSE(int[] heights) {
-        int n = heights.length;
-        int[] pse = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
-            }
-            pse[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
-        }
-
-        return pse;
     }
 }
