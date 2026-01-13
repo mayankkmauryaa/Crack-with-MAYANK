f9593e6faee89df0b00769e4da193ce24ae0ec71
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-13 21:58:31 +0530

    Time: 0 ms (100%), Space: 43.8 MB (82.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,13 +1,35 @@
 class Solution {
+
+    // The first integer of each row is greater than the last integer of the previous row.
+
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int i=0;
-        int j=n-1;
-        while(i<m && j>=0){
-            if(matrix[i][j]==target) return true;
-            if(matrix[i][j]>target) j--;
-            else i++;
-        } return false;
+
+        int low = 0;
+
+        // O(log(m × n))
+        int high = m * n - 1; // total items
+
+        while (low <= high) {
+            // index in the imaginary 1D array
+            int mid = low + (high - low) / 2;
+
+            // we choose n here because sorted array is split up into n pieces {columns}
+            int row = mid / n;
+            int col = mid % n;
+
+            int value = matrix[row][col];
+
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
