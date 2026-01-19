5fdc87bcab4d87a3ba7c0e0acd75750a8fc83d08
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-19 11:35:19 +0530

    Time: 4 ms (83.5%), Space: 44.6 MB (86.64%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        char[] x = s.toCharArray();
+        for (char c : x) {
+            if (c == 'B' && !st.isEmpty() && st.peek() == 'A') {
+                st.pop();
+            } else if (c == 'D' && !st.isEmpty() && st.peek() == 'C') {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.size();
+    }
+}
