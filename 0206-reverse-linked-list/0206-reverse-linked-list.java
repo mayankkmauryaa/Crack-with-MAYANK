3a42d0619cb776b5ebf46aaec798888a92dee286
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-19 21:30:25 +0530

    Time: 0 ms (100%), Space: 44.6 MB (7.41%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,37 +10,14 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode temp = head;
-        ListNode prev = null;
-        while(temp!=null) { 
-            ListNode front = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = front;
-        }
-        return prev;
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
-
-/** 
-temp = head
-prev = null
-
-
-temp -> next = prev
-but this breaks the previous link so we'll store the next in other node
-let say : front = temp -> next
-then : temp -> next = prev
-
-
-now moving the temp to front place for next iteration
-temp = front
-but this also makes lost the prev
-so we'll make sure to update the prev
-by : prev = temp
-then : temp = front
-
-
-
-put all this in while unitl the temp becomes null
-*/
