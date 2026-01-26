88d196b2caece7e515719a623ae2261d93cc64a7
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-27 02:53:57 +0530

    Time: 0 ms (100%), Space: 47.4 MB (41.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -10,3 +10,26 @@ class Solution {
         return xor;
     }
 }
+
+
+class Solution_maths {
+    public int missingNumber(int[] nums) {
+        int n = nums.length + 1;
+        int total = (n * (n - 1)) / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        return total - sum;
+    }
+}
+
+class Solution_prefix {
+    public int missingNumber(int[] nums) {
+        int ans = nums.length;
+        for (int i=0; i<nums.length; i++)  ans+=i-nums[i];
+        return ans;
+    }
+}
+
