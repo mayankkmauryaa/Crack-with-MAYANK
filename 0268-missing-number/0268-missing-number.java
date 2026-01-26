c48ec614f576c24ac73496fdd3ff75f82fe2165a
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-27 02:58:54 +0530

    Time: 0 ms (100%), Space: 47.7 MB (17.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -11,7 +11,6 @@ class Solution {
     }
 }
 
-
 class Solution_maths {
     public int missingNumber(int[] nums) {
         int n = nums.length + 1;
@@ -25,11 +24,43 @@ class Solution_maths {
     }
 }
 
-class Solution_prefix {
+class Solution_runningSum {
     public int missingNumber(int[] nums) {
         int ans = nums.length;
-        for (int i=0; i<nums.length; i++)  ans+=i-nums[i];
+        for (int i = 0; i < nums.length; i++)
+            ans += i - nums[i];
         return ans;
     }
 }
 
+class Solution_reposition {
+    public int missingNumber(int[] nums) { // resposition in another array a/c to idx
+        int n = nums.length;
+        int arr[] = new int[n + 1];
+        Arrays.fill(arr, -1);
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]] = nums[i];
+        }
+        for (int i = 0; i < n + 1; i++) {
+            if (arr[i] == -1)
+                return i;
+        }
+        return 0;
+    }
+}
+
+class Solution_sort {
+    public int missingNumber(int[] nums) { // sort -> edge case -> traverse
+        Arrays.sort(nums);
+        int n = nums.length;
+        if (nums[0] != 0)
+            return 0;
+        if (nums[n - 1] != n)
+            return n;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != i)
+                return i;
+        }
+        return 0;
+    }
+}
