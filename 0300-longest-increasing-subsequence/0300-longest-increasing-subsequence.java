f7af7ea47a8e543603fd8de97ec2fa19269b747f
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-29 00:21:28 +0530

    Time: 36 ms (75.72%), Space: 45.6 MB (97.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -22,3 +22,25 @@ class Solution {
         return ans;
     }
 }
+
+class Solution_Recursion {
+    public int lengthOfLIS(int[] nums) {
+        return dfs(nums, 0, -1);
+    }
+
+    private int dfs(int[] nums, int index, int prevIndex) {
+        if (index == nums.length)
+            return 0;
+
+        // skip current element
+        int notTake = dfs(nums, index + 1, prevIndex);
+
+        // take current element if increasing
+        int take = 0;
+        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
+            take = 1 + dfs(nums, index + 1, index);
+        }
+
+        return Math.max(take, notTake);
+    }
+}
