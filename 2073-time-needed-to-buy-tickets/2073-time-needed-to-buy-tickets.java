473809e3520c8128232cd3648d1a7c8203bdbbc4
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-29 11:49:23 +0530

    Time: 2 ms (50.46%), Space: 43.1 MB (65.32%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -13,3 +13,23 @@ class Solution {
         return time;
     }
 }
+
+class Solution_Queue {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q = new LinkedList();
+        int n = tickets.length;
+        for (int i = 0; i < n; i++) {
+            q.add(i);
+        }
+        int time = 0;
+        while (tickets[k] != 0) {
+            int buy = q.remove();
+            tickets[buy]--;
+            time++;
+            if (tickets[buy] != 0) {
+                q.add(buy);
+            }
+        }
+        return time;
+    }
+}
