6e834cf73ad43a61ff27e637f3f3d54947d4b58a
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-31 23:32:17 +0530

    Time: 0 ms (100%), Space: 46.5 MB (43.82%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,8 +1,24 @@
-class Solution {
+class Solution_linear {
     public char nextGreatestLetter(char[] letters, char target) {
         for (int i = 0; i < letters.length; i++)
             if (target < letters[i])
                 return letters[i];
         return letters[0];
+    }
+}
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int s = 0;
+        int e = n - 1;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (target < letters[m])
+                e = m - 1;
+            else
+                s = m + 1;
+        }
+        return letters[s % n];
     }
 }
