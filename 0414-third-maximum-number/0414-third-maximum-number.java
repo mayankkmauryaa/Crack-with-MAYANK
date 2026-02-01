2626d6c3fe12f0f53daa751cb4bfde21ba70da6d
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-01 23:08:45 +0530

    Time: 1 ms (98.64%), Space: 44.7 MB (79.23%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -21,3 +21,32 @@ class Solution {
         return third == Long.MIN_VALUE ? (int) first : (int) third;
     }
 }
+
+class Solution_TreeSet {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int n : nums) {
+            set.add(n);
+            if (set.size() > 3) {
+                set.pollFirst();
+            }
+        }
+
+        return set.size() == 3 ? set.first() : set.last();
+    }
+}
+
+class Solution_HashSet_Sorting {
+    public int thirdMax(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums)
+            set.add(n);
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        int size = list.size();
+        return size >= 3 ? list.get(size - 3) : list.get(size - 1);
+    }
+}
