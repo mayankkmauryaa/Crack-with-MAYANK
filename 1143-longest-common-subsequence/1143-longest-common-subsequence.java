3697d6bc308956bb840b990abebd5884aa2a331b
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-20 00:21:24 +0530

    Time: 20 ms (78.8%), Space: 55.8 MB (6.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,3 +1,58 @@
+class Solution_Recursion {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        return lcs(text1, text2, m, n);
+    }
+
+    public static int lcs(String s1, String s2, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return 1 + lcs(s1, s2, m - 1, n - 1);
+        }
+
+        return Math.max(
+                lcs(s1, s2, m - 1, n),
+                lcs(s1, s2, m, n - 1));
+    }
+}
+
+class Solution_Memoization {
+    static int memo[][];
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        memo = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        return lcs(text1, text2, m, n);
+    }
+
+    public static int lcs(String s1, String s2, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+
+        // memo
+        if (memo[m][n] != -1)
+            return memo[m][n];
+
+        // rec calls
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1);
+        }
+
+        return memo[m][n] = Math.max(
+                lcs(s1, s2, m - 1, n),
+                lcs(s1, s2, m, n - 1));
+    }
+}
+
+// Tabulation / Bottom-Up
 class Solution {
     static int dp[][];
 
@@ -5,13 +60,6 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++)
-            Arrays.fill(dp[i], -1);
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = 0;
-        for (int i = 0; i <= n; i++)
-            dp[0][i] = 0;
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
