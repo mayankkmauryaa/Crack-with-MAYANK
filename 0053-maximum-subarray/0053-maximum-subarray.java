e1aeec2e16f270c17d02622524d5fa36ab196b09
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-20 00:25:46 +0530

    Time: 1 ms (99.67%), Space: 77.3 MB (66.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,29 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxsum = nums[0];
-        int n = nums.length;
-        int sum = nums[0];
-        for (int i = 1; i < n; i++) {
-            sum = Math.max(nums[i], sum + nums[i]);
-            // sum chooses
-            // Either start a new subarray at i (nums[i]), or
-            // Extend the previous subarray (sum + nums[i]).
-            maxsum = Math.max(maxsum, sum);
-        }
-        return maxsum;
-    }
+        int max = nums[0];
+        int maxx = nums[0];
 
-    public int Kadanes(int[] nums) {
-        int maxsum = Integer.MIN_VALUE;
-        int n = nums.length;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            maxsum = Math.max(maxsum, sum);
-            // If sum becomes negative, it cannot help in forming a maximum subarray in the future → reset sum = 0.
-            if (sum < 0)
-                sum = 0;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(nums[i], max + nums[i]);
+            maxx = Math.max(maxx, max);
         }
-        return maxsum;
+
+        return maxx;
     }
 }
