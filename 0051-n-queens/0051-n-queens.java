6fac96dd80a2d4fa78ab586f6403575901ed7e3e
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-22 23:53:30 +0530

    Time: 3 ms (45%), Space: 46.6 MB (82.96%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    List<List<String>> result;
+    int[][] board;
+
+    public List<List<String>> solveNQueens(int n) {
+        result = new ArrayList<>();
+        board = new int[n][n];
+        solve(0, n);
+        return result;
+    }
+
+    private void solve(int row, int n) {
+        if (row == n) {
+            result.add(construct(n));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, n)) {
+                board[row][col] = 1;
+                solve(row + 1, n);
+                board[row][col] = 0; // backtrack
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int col, int n) {
+
+        // Check column
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 1)
+                return false;
+
+        // Check left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        // Check right diagonal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    private List<String> construct(int n) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb.append(board[i][j] == 1 ? 'Q' : '.');
+            }
+            list.add(sb.toString());
+        }
+
+        return list;
+    }
+}
