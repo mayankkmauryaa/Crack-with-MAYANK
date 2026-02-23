171ebb74f8df01cc2234f85f82069fc4ef65f24c
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-23 23:20:47 +0530

    Time: 7 ms (15.28%), Space: 51.5 MB (88.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,57 +1,52 @@
 class Pair {
-    int first;
-    int second;
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
+    int val1;
+    int val2;
+
+    public Pair(int val1, int val2) {
+        this.val1 = val1;
+        this.val2 = val2;
     }
 }
 
 class Solution {
+    int n;
+    int m;
+    boolean vis[][];
+
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean vis[][] = new boolean[n][m];
+        n = grid.length;
+        m = grid[0].length;
+        vis = new boolean[n][m];
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                    if (!vis[i][j] && grid[i][j] == '1') {          // visited checking & must be land
-                    count++;                // 1 land counts
-                    bfs(i, j, grid, vis);       // get through every connected land
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    count++;
+                    bfs(i, j, grid);
                 }
             }
         }
         return count;
     }
 
-    private void bfs(int r, int c, char[][] mat, boolean[][] vis) {
+    private void bfs(int r, int c, char[][] grid) {
         vis[r][c] = true;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(r, c));
 
-        int n = mat.length; // row
-        int m = mat[0].length; // col
-
         while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
+            int row = q.peek().val1;
+            int col = q.peek().val2;
             q.poll();
-
-            // traverse in the neighbours & mark them if its a land
-            // varying from -1 to +1
-
             for (int rx = -1; rx <= 1; rx++) {
                 for (int cx = -1; cx <= 1; cx++) {
-                    if (Math.abs(rx + cx) == 1) { // 4 directional only
+                    if (Math.abs(rx + cx) == 1) {
 
-                    // n_row = neighbour_row ....etc
                         int n_row = row + rx;
                         int n_col = col + cx;
 
-                        // validation
-
-                        if (n_row >= 0 && n_row < n && n_col >= 0 && n_col < m) { // region check
-                            if (mat[n_row][n_col] == '1' && !vis[n_row][n_col]) { // water & land check then visited or not checking
+                        if (n_row >= 0 && n_row < n && n_col >= 0 && n_col < m) {
+                            if (grid[n_row][n_col] == '1' && !vis[n_row][n_col]) {
                                 vis[n_row][n_col] = true;
                                 q.add(new Pair(n_row, n_col));
                             }
