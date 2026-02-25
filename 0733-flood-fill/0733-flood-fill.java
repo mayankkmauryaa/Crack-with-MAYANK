829271429c464960e7436c4ecb78ea847910d8f6
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-26 03:35:09 +0530

    Time: 0 ms (100%), Space: 46.8 MB (64.3%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,4 @@
 class Solution {
-    // by don't changing the data 
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int initial = image[sr][sc];
-        int[][] ans = image;
-        dfs(image, ans, sr, sc, color, initial);
-        return ans;
-    }
-
-    public void dfs(int[][] mat, int[][] ans, int sr, int sc, int color, int initial) {
-        int n = mat.length;
-        int m = mat[0].length;
-        ans[sr][sc] = color;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (Math.abs(i + j) == 1) {
-                    int nr = sr + i;
-                    int nc = sc + j;
-                    if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                        if (ans[nr][nc] != color && mat[nr][nc] == initial) {
-                            dfs(mat, ans, nr, nc, color, initial);
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     // using visitied array
     public int[][] floodFill1(int[][] image, int sr, int sc, int color) {
@@ -51,6 +25,33 @@ class Solution {
                     if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
                         if (!vis[nr][nc] && mat[nr][nc] == initial) {
                             dfs(mat, nr, nc, color, vis, initial);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // by don't changing the data 
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int initial = image[sr][sc];
+        int[][] ans = image;
+        dfs(image, ans, sr, sc, color, initial);
+        return ans;
+    }
+
+    public void dfs(int[][] mat, int[][] ans, int sr, int sc, int color, int initial) {
+        int n = mat.length;
+        int m = mat[0].length;
+        ans[sr][sc] = color;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (Math.abs(i + j) == 1) {
+                    int nr = sr + i;
+                    int nc = sc + j;
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                        if (ans[nr][nc] != color && mat[nr][nc] == initial) {
+                            dfs(mat, ans, nr, nc, color, initial);
                         }
                     }
                 }
