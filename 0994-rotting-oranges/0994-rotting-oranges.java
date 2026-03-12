5223c2265183247e7c670bdfa56ba7bf2f9af419
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-03-13 01:38:26 +0530

    Time: 2 ms (90.73%), Space: 44.3 MB (53.02%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,51 +1,59 @@
+class Pair {
+    int first;
+    int second;
+
+    Pair(int x, int y) {
+        this.first = x;
+        this.second = y;
+    }
+}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        Queue<int[]> q = new LinkedList<>();
+        Queue<Pair> q = new LinkedList<>();
         int fresh = 0;
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2)
-                    q.add(new int[] { i, j });
+                    q.add(new Pair(i, j));
                 else if (grid[i][j] == 1)
                     fresh++;
             }
         }
-
         if (fresh == 0)
             return 0;
 
         int time = 0;
         while (!q.isEmpty()) {
-            int size = (q.size());
-            boolean rottenNOW = false;
+            int size = q.size();
+            boolean rottenNow = false;
 
             for (int s = 0; s < size; s++) {
-                int[] next = q.poll();
-                int row = next[0];
-                int col = next[1];
+                int x = q.peek().first;
+                int y = q.peek().second;
+                q.poll();
 
                 for (int i = -1; i <= 1; i++) {
                     for (int j = -1; j <= 1; j++) {
                         if (Math.abs(i + j) == 1) {
-                            int nr = row + i;
-                            int nc = col + j;
-                            if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                                if (grid[nr][nc] == 1) {
-                                    grid[nr][nc] = 2;
+                            int nx = x + i;
+                            int ny = y + j;
+
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                                if (grid[nx][ny] == 1) {
+                                    grid[nx][ny] = 2;
+                                    q.add(new Pair(nx, ny));
                                     fresh--;
-                                    q.add(new int[] { nr, nc });
-                                    rottenNOW = true;
+                                    rottenNow = true;
                                 }
                             }
                         }
                     }
                 }
             }
-            if (rottenNOW)
+            if (rottenNow)
                 time++;
         }
         return fresh == 0 ? time : -1;
