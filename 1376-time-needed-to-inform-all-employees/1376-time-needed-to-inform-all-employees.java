512fe5222369beb1b78056fb86a43663198aaa15
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-03-14 02:59:08 +0530

    Time: 50 ms (78.53%), Space: 127.3 MB (57.34%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,35 +1,22 @@
-class Pair {
-    int node;
-    int time;
-
-    Pair(int x, int y) {
-        this.node = x;
-        this.time = y;
-    }
-}
 
 class Solution {
+    List<List<Integer>> adj = new ArrayList<>();
+
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++)
             adj.add(new ArrayList<>());
         for (int i = 0; i < n; i++) {
             if (manager[i] != -1)
                 adj.get(manager[i]).add(i);
         }
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(headID, 0));
-        int maxTime = 0;
-        while (!q.isEmpty()) {
-            Pair next = q.poll();
-            int node = next.node;
-            int time = next.time;
-            maxTime = Math.max(maxTime, time);
+        return dfs(headID, informTime);
+    }
 
-            for (int neigh : adj.get(node)) {
-                q.add(new Pair(neigh, time + informTime[node]));
-            }
+    public int dfs(int root, int[] time) {
+        int max = 0;
+        for (int neigh : adj.get(root)) {
+            max = Math.max(max, dfs(neigh, time));
         }
-        return maxTime;
+        return time[root] + max;
     }
 }
