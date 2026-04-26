2fdc07abf98fa73d0e8db1e39f12122740af3927
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-04-26 15:53:07 +0530

    Time: 1 ms (100%), Space: 46.7 MB (62.28%) - LeetHub

--- 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
@@ -1,30 +1,23 @@
 class Solution {
     public List<Integer> findValidElements(int[] nums) {
-        int n = nums.length;
-        HashSet<Integer> set = new HashSet<>(); 
-
-        set.add(0);
-        int max = nums[0];
-        for(int i=1; i<n; i++){
-            if(max<nums[i]){
-                set.add(i);
-                max = nums[i];
-            }
-        }
-
-        set.add(n-1);
-        max = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            if(max<nums[i]){
-                set.add(i);
-                max = nums[i];
-            }
-        }
-
         List<Integer> valid = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            if(set.contains(i)) valid.add(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            boolean f1 = true, f2 = true;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= nums[i]) {
+                    f1 = false;
+                    break;
+                }
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] >= nums[i]) {
+                    f2 = false;
+                    break;
+                }
+            }
+            if (f1 || f2) valid.add(nums[i]);
         }
+
         return valid;
     }
 }
