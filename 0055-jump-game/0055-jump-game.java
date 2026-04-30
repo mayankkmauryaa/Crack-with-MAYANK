5b783a73a9ff781bc85b92fd1bf6e2c8ad0e3657
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-04-30 22:03:19 +0530

    Time: 2 ms (86.85%), Space: 48 MB (41.5%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         int jump = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i > jump)
-                return false;
+            if (i > jump) return false;
             jump = Math.max(jump, i + nums[i]);
+            if(jump >= nums.length) return true;
         }
         return true;
     }
