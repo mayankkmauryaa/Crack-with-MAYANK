44855cb570273f0ea7054618a541645473c9e7dd
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-05-06 23:01:50 +0530

    Time: 93 ms (5.08%), Space: 45.1 MB (13.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,20 @@
 class Solution {
+    List<String> ans = new ArrayList<String>();
+
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<String>();
-        generate(ans, 0, 0, "", n);
+        generate(0, 0, "", n);
         return ans;
     }
-    
-    public void generate(List<String> ans, int opn, int cls, String s, int n) {
-        if (opn == n && cls == n){
+
+    public void generate(int opn, int cls, String s, int n) {
+        if (opn == n && cls == n) {
             ans.add(s);
             return;
-        } 
-        if (opn < n) generate(ans, opn + 1, cls, s + "(", n);        
-        if (cls < opn) generate(ans, opn, cls + 1, s + ")", n);
+        }
+        if (opn < n)
+            generate(opn + 1, cls, s + "(", n);
+        if (cls < opn)
+            generate(opn, cls + 1, s + ")", n);
     }
 }
 // See tree diagram (given below) with parameters (opn, cls, s) for better understanding
