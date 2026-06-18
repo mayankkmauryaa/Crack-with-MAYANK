83001b9ff58e22c0c4248b339e3ea0077339055e
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-06-18 08:10:42 +0530

    Time: 0 ms (100%), Space: 44 MB (23.71%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -5,31 +5,20 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-
         int low = 0;
-        
         // O(log(m × n))
         int high = m * n - 1;   // total items
-
         while (low <= high) {
             // index in the imaginary 1D array
             int mid = low + (high - low) / 2;
-
             // we choose n here because sorted array is split up into n pieces {columns}
             int row = mid / n;
             int col = mid % n;
-
             int value = matrix[row][col];
-
-            if (value == target) {
-                return true;
-            } else if (value < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            if (value == target) return true;
+            else if (value < target) low = mid + 1;
+            else high = mid - 1;
         }
-
         return false;
     }
 }
@@ -42,12 +31,9 @@ class Worst_Case_Solution {
         int i = 0;
         int j = n - 1;
         while (i < m && j >= 0) {
-            if (matrix[i][j] == target)
-                return true;
-            if (matrix[i][j] > target)
-                j--;
-            else
-                i++;
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target) j--;
+            else i++;
         }
         return false;
     }
