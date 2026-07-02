b1bfed4775a452b18409da3869c43a07d6cd92cc
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-07-02 17:08:13 +0530

    Time: 1 ms (100%), Space: 52.3 MB (58.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,59 +1,40 @@
-class Pair {
-    int val1;
-    int val2;
-
-    public Pair(int val1, int val2) {
-        this.val1 = val1;
-        this.val2 = val2;
-    }
-}
-
 class Solution {
-    int n;
-    int m;
-    boolean vis[][];
+    int rows = 0, cols = 0, count = 0;
 
     public int numIslands(char[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        vis = new boolean[n][m];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
-                    count++;
-                    bfs(i, j, grid);
-                }
-            }
+        rows = grid.length;
+        cols = grid[0].length;
+        count = 0;
+
+        for (int row = 0; row < rows; row++) {
+            check(row, grid);
         }
+
         return count;
     }
 
-    private void bfs(int r, int c, char[][] grid) {
-        vis[r][c] = true;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(r, c));
-
-        while (!q.isEmpty()) {
-            int row = q.peek().val1;
-            int col = q.peek().val2;
-            q.poll();
-            for (int rx = -1; rx <= 1; rx++) {
-                for (int cx = -1; cx <= 1; cx++) {
-                    if (Math.abs(rx + cx) == 1) {
-
-                        int n_row = row + rx;
-                        int n_col = col + cx;
-
-                        if (n_row >= 0 && n_row < n && n_col >= 0 && n_col < m) {
-                            if (grid[n_row][n_col] == '1' && !vis[n_row][n_col]) {
-                                vis[n_row][n_col] = true;
-                                q.add(new Pair(n_row, n_col));
-                            }
-                        }
-                    }
-                }
+    private void check(int row, char[][] grid) {
+        for (int col = 0; col < cols; col++) {
+            if (grid[row][col] == '1') {
+                count++;
+                dfs(row, col, grid);
             }
         }
+    }
+
+    private void dfs(int row, int col, char[][] grid) {
+        grid[row][col] = '*';
+
+        if (row - 1 >= 0 && grid[row - 1][col] == '1')
+            dfs(row - 1, col, grid);
+
+        if (row + 1 < rows && grid[row + 1][col] == '1')
+            dfs(row + 1, col, grid);
+
+        if (col - 1 >= 0 && grid[row][col - 1] == '1')
+            dfs(row, col - 1, grid);
+
+        if (col + 1 < cols && grid[row][col + 1] == '1')
+            dfs(row, col + 1, grid);
     }
 }
