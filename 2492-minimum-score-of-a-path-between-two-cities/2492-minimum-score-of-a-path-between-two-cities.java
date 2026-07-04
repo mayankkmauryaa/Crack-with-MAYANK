21094ef5ec8682b30ad1436a41de6723ce1e62b0
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-07-04 11:44:16 +0530

    Time: 27 ms (64.91%), Space: 162.7 MB (48.9%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -9,6 +9,40 @@ class Pair {
 }
 
 class Solution {
+    public int minScore(int n, int[][] roads) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
+
+            adj.get(u).add(new Pair(v, w));
+            adj.get(v).add(new Pair(u, w));
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[n + 1];
+        q.add(1);
+        vis[1] = true;
+        int ans = Integer.MAX_VALUE;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (Pair p : adj.get(node)) {
+                ans = Math.min(ans, p.y);
+                if (!vis[p.x]) {
+                    vis[p.x] = true;
+                    q.add(p.x);
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution_DFS {
     boolean vis[];
     List<List<Pair>> adj;
     int ans = Integer.MAX_VALUE;
