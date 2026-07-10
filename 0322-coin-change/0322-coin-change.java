1e2cf0d69463a896ae0d7311db6b7333ee43181c
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-07-11 00:23:48 +0530

    Time: 10 ms (99.75%), Space: 46.1 MB (92.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,12 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Map<Integer, Integer> map = new HashMap<>();
-        return coinChange(coins, amount, map);
-    }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 1, dp.length, amount + 1);
 
-    private int coinChange(int[] coins, int amount, Map<Integer, Integer> mem) {
-        if (amount < 0)
-            return -1;
-        if (amount == 0)
-            return 0;
-        Integer c = mem.get(amount);
-        if (c != null)
-            return c;
-        int cc = -1;
-        for (int i = 0; i < coins.length; i++) {
-            int coin = coinChange(coins, amount - coins[i], mem);
-            if (coin >= 0)
-                cc = cc < 0 ? coin + 1 : Math.min(cc, coin + 1);
-        }
-        mem.put(amount, cc);
-        return cc;
+        for (final int coin : coins)
+            for (int i = coin; i <= amount; ++i)
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
