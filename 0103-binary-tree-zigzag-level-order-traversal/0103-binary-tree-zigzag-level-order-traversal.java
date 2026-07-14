1cc606576a95930e39962579b1fe63e419675b66
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-07-14 18:55:37 +0530

    Time: 1 ms (80.3%), Space: 43.9 MB (25.24%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,24 +15,31 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> bt = new ArrayList<>();
-        if (root == null) return bt;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Deque<TreeNode> dq = new LinkedList<>();
+        dq.add(root);
         boolean rev = true;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            LinkedList<Integer> level = new LinkedList<>();
+        while (!dq.isEmpty()) {
+            int size = dq.size();
+            List<Integer> level = new ArrayList<>();
             for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if (rev) level.addLast(node.val);
-                else level.addFirst(node.val);
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
+                if (rev) {
+                    TreeNode node = dq.pollFirst();
+                    level.add(node.val);
+                    if (node.left != null) dq.addLast(node.left);
+                    if (node.right != null) dq.addLast(node.right);
+
+                } else {
+                    TreeNode node = dq.pollLast();
+                    level.add(node.val);
+                    if (node.right != null) dq.addFirst(node.right);
+                    if (node.left != null) dq.addFirst(node.left);
+                }
             }
-            bt.add(level);
+            ans.add(level);
             rev = !rev;
         }
-        return bt;
+        return ans;
     }
 }
