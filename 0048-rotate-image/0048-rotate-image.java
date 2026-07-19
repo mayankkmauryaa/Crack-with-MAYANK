6ee4e9c7df48375873356a4e8ad580c92ae15ebe
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-07-19 13:13:43 +0530

    Time: 0 ms (100%), Space: 43.9 MB (16.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_with_space {
     public void rotate(int[][] matrix) {
         // approach 1 : brute force :  using space -> O(n2)
         int n = matrix.length;
@@ -11,6 +11,27 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = ans[i][j];
+            }
+        }
+    }
+}
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        // aproach 2 : optimal : transposing -> reverse rows
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
             }
         }
     }
