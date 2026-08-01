9b117e329789a6929e33cb46555b694e06915e6c
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-08-01 21:42:06 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,12 +1,20 @@
 class Solution {
+    int[][] memo;
+
     public boolean predictTheWinner(int[] nums) {
-        return solve(0, nums.length - 1, nums) >= 0;
+        int n = nums.length;
+        memo = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                memo[i][j] = -1;
+        return solve(0, n - 1, nums) >= 0;
     }
 
     private int solve(int i, int j, int[] nums) {
         if (i == j) return nums[i];
-        int takeLeft = nums[i] - solve(i + 1, j, nums);
-        int takeRight = nums[j] - solve(i, j - 1, nums);
-        return Math.max(takeLeft, takeRight);
+        if (memo[i][j] != -1) return memo[i][j];
+        int left = nums[i] - solve(i + 1, j, nums);
+        int right = nums[j] - solve(i, j - 1, nums);
+        return memo[i][j] = Math.max(left, right);
     }
 }
