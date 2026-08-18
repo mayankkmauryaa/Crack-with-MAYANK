1746411cccf687892b25cf60a97e8b457149476d
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-08-18 11:12:03 +0530

    Time: 2 ms (72.63%), Space: 45.1 MB (53.33%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,24 +1,34 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
-        int[] count = new int[51];
-
-        for (int i = 0; i + k <= n; i++) {
-            Set<Integer> set = new HashSet<>();
-            for (int j = i; j < i + k; j++) {
-                set.add(nums[j]);
-            }
-            for (int x : set) {
-                count[x]++;
-            }
+        if (k > n)
+            return -1;
+        if (k == n) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++)
+                max = Math.max(max, nums[i]);
+            return max;
         }
-
-        int ans = -1;
-        for (int v = 0; v <= 50; v++) {
-            if (count[v] == 1) {
-                ans = Math.max(ans, v);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        if (k == 1) {
+            int max = -1;
+            for (int i = 0; i < n; i++) {
+                if (map.get(nums[i]) == 1 && nums[i] > max)
+                    max = nums[i];
             }
+            return max;
         }
-        return ans;
+        n = n - 1;
+        if (nums[0] == nums[n])
+            return -1;
+        if (map.get(nums[0]) == 1 && map.get(nums[n]) == 1)
+            return Math.max(nums[0], nums[n]);
+        if (map.get(nums[0]) == 1 && map.get(nums[n]) > 1)
+            return nums[0];
+        if (map.get(nums[n]) == 1 && map.get(nums[0]) > 1)
+            return nums[n];
+        return -1;
     }
 }
