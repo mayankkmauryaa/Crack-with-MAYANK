13b3babda77861c784ef5bed62bf6653fc54ad69
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-08-26 22:09:30 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.51%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,15 +1,5 @@
 class Solution {
     public String convertToBase7(int num) {
-        if (num == 0) return "0";
-        boolean negative = num < 0;
-        num = Math.abs(num);
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            int rem = num % 7;
-            sb.append(rem);
-            num /= 7;
-        }
-        if (negative) sb.append('-');
-        return sb.reverse().toString();
+        return Integer.toString(num, 7);
     }
 }
