5bc8a235dc45bd60e47f2a7f1636eaf6fb22e40b
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-08-26 22:11:22 +0530

    Time: 0 ms (100%), Space: 43 MB (11.18%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -3,3 +3,19 @@ class Solution {
         return Integer.toString(num, 7);
     }
 }
+
+class Solution_ {
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+        boolean negative = num < 0;
+        num = Math.abs(num);
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            int rem = num % 7;
+            sb.append(rem);
+            num /= 7;
+        }
+        if (negative) sb.append('-');
+        return sb.reverse().toString();
+    }
+}
