bfe244af91c16e4613ddc56c02c7e215cf1eef1b
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-09-08 16:28:17 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,27 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        if (n < 1000)
-            return 0;
-        return n - 999;
+        return (n >= 1000) ? (n - 999) : 0;
     }
 }
-
-// class Solution {
-//     public long countCommas(long n) {
-//         long ans = 0;
-
-//         // Numbers with 1 comma: 1000 to 999999
-//         if (n >= 1000) {
-//             long count = Math.min(n, 999999) - 1000 + 1;
-//             ans += count;
-//         }
-
-//         // Numbers with 2 commas: 1000000 to 999999999
-//         if (n >= 1000000) {
-//             long count = Math.min(n, 999999999) - 1000000 + 1;
-//             ans += count * 2;
-//         }
-
-//         return ans;
-//     }
-// }
