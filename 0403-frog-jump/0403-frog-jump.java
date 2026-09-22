1b641094b017b54a40456e4ed2251900910084fd
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-09-22 16:44:35 +0530

    Time: 11 ms (96.64%), Space: 198.2 MB (11.07%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,36 +1,27 @@
 class Solution {
-
     Boolean[][] memo;
+    int n;
 
     public boolean canCross(int[] stones) {
-
-        memo = new Boolean[stones.length][stones.length + 1];
-
+        n = stones.length;
+        memo = new Boolean[n][n + 1];
         return dfs(stones, 0, 0);
     }
 
     private boolean dfs(int[] stones, int index, int lastJump) {
-        if (index == stones.length - 1) return true;
-
+        if (index == n - 1) return true;
         if (memo[index][lastJump] != null) return memo[index][lastJump];
 
-        for (int jump = lastJump - 1;
-             jump <= lastJump + 1;
-             jump++) {
-
+        for (int jump = lastJump - 1; jump <= lastJump + 1; jump++) {
             if (jump <= 0) continue;
 
             int nextPosition = stones[index] + jump;
 
-            for (int next = index + 1;
-                 next < stones.length;
-                 next++) {
-
+            for (int next = index + 1; next < n; next++) {
                 if (stones[next] == nextPosition) {
                     if (dfs(stones, next, jump)) return memo[index][lastJump] = true;
                     break;
                 }
-
                 if (stones[next] > nextPosition) break;
             }
         }
